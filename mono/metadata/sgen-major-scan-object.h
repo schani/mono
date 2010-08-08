dfc94f0678ac8a2b48f45e13cac318b2f5fa0fcc
Rodrigo Kumpera <kumpera@gmail.com>
2010-08-08 15:51:19 -0300

    Direct card table implementation. No managed wb yet.
    Only done for sequential m&s.

--- mono/metadata/sgen-major-scan-object.h
+++ mono/metadata/sgen-major-scan-object.h
@@ -10,7 +10,7 @@ extern long long stat_scan_object_called_major;
 			__copy = *(ptr);	\
 			DEBUG (9, if (__old != __copy) fprintf (gc_debug_file, "Overwrote field at %p with %p (was: %p)\n", (ptr), *(ptr), __old));	\
 			if (G_UNLIKELY (ptr_in_nursery (__copy) && !ptr_in_nursery ((ptr)))) \
-				mono_sgen_add_to_global_remset ((ptr));	\
+				sgen_card_table_mark_address ((mword)(ptr));	\
 		}	\
 	} while (0)
 
@@ -74,7 +74,7 @@ minor_scan_vtype (char *start, mword desc, char* from_start, char* from_end, Sge
 			__copy = *(ptr);				\
 			DEBUG (9, if (__old != __copy) mono_sgen_debug_printf (9, "Overwrote field at %p with %p (was: %p)\n", (ptr), *(ptr), __old)); \
 			if (G_UNLIKELY (ptr_in_nursery (__copy) && !ptr_in_nursery ((ptr)))) \
-				mono_sgen_add_to_global_remset ((ptr));	\
+				sgen_card_table_mark_address ((mword)(ptr));	\
 		}							\
 	} while (0)
 
