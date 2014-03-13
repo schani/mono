ee90afe14ac63839e1fd10b5b4bc272db14cce32
Mark Probst <mark.probst@gmail.com>
2014-03-13 16:08:55 -0700

    DEBUG: Beef up the LLS unit test with thread stopping.
    Stops threads for a while.  My guess was that maybe the interruptions
    from regular multithreading weren't long enough to surface the bug.
    The stops didn't trigger the bug.
    
    I also put in thread-local items.  Set LOCAL_NODES_PER_THREAD to 1 to
    enable it, in which case every thread has one node that only that
    thread will touch.  Also doesn't trigger the bug.

--- mono/unit-tests/test-mono-linked-list-set.c
+++ mono/unit-tests/test-mono-linked-list-set.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <pthread.h>
+#include <signal.h>
 
 #include <config.h>
 #include <mono/metadata/metadata.h>
@@ -16,16 +17,19 @@ enum {
 	STATE_IN
 };
 
-#define N 11
-#define NUM_ITERS 1000000
-#define NUM_THREADS 4
+#define N 23
+#define NUM_ITERS 10000000
+#define NUM_THREADS 8
+#define LOCAL_NODES_PER_THREAD 0
 
 typedef struct {
 	MonoLinkedListSetNode node;
+	int tid;		/* owner thread, or -1 if it's global */
 	int state;
 } node_t;
 
 typedef struct {
+	int tid;
 	int skip;
 	int num_adds;
 	int num_removes;
@@ -53,6 +57,41 @@ free_node (void *n)
 	node->state = STATE_OUT;
 }
 
+static gboolean
+change_state (int *state, gboolean local, int new, int old)
+{
+	if (local) {
+		assert (*state == old);
+		*state = new;
+		return TRUE;
+	}
+	return InterlockedCompareExchange (state, new, old) == old;
+}
+
+static void
+signal_handler (int signum)
+{
+	//g_print ("signal!\n");
+	g_usleep (75000);
+}
+
+static void
+set_signal_handler (void)
+{
+	int result;
+	/*
+	struct sigaction sa;
+
+	sa.sa_handler = signal_handler;
+	sa.sa_sigaction = NULL;
+	sigfillset (&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	result = sigaction (SIGXCPU, &sa, NULL);
+	*/
+	result = signal (SIGXCPU, signal_handler) == SIG_ERR;
+	assert (!result);
+}
+
 static void*
 worker (void *arg)
 {
@@ -62,18 +101,24 @@ worker (void *arg)
 	int i, j;
 	gboolean result;
 
+	//set_signal_handler ();
+
 	mono_thread_info_register_small_id ();
 
 	hp = mono_hazard_pointer_get ();
 
 	i = 0;
 	for (j = 0; j < NUM_ITERS; ++j) {
+		gboolean local = nodes [i].tid == thread_data->tid;
+		if (!local && nodes [i].tid >= 0)
+			goto skip_over;
+
 		switch (nodes [i].state) {
 		case STATE_BUSY:
 			mono_thread_hazardous_try_free_some ();
 			break;
 		case STATE_OUT:
-			if (InterlockedCompareExchange (&nodes [i].state, STATE_BUSY, STATE_OUT) == STATE_OUT) {
+			if (change_state (&nodes [i].state, local, STATE_BUSY, STATE_OUT)) {
 				result = mono_lls_find (&lls, hp, i);
 				assert (!result);
 				mono_hazard_pointer_clear_all (hp, -1);
@@ -88,7 +133,7 @@ worker (void *arg)
 			}
 			break;
 		case STATE_IN:
-			if (InterlockedCompareExchange (&nodes [i].state, STATE_BUSY, STATE_IN) == STATE_IN) {
+			if (change_state (&nodes [i].state, local, STATE_BUSY, STATE_IN)) {
 				result = mono_lls_find (&lls, hp, i);
 				assert (result);
 				assert (mono_hazard_pointer_get_val (hp, 1) == &nodes [i].node);
@@ -104,9 +149,17 @@ worker (void *arg)
 			assert (FALSE);
 		}
 
+		skip_over:
 		i += skip;
-		if (i >= N)
+		if (i >= N) {
+			node_t *node;
+
 			i -= N;
+
+			MONO_LLS_FOREACH_SAFE (&lls, node, node_t*) {
+				assert (node->state != STATE_OUT);
+			} MONO_LLS_END_FOREACH_SAFE;
+		}
 	}
 
 	return NULL;
@@ -115,10 +168,10 @@ worker (void *arg)
 int
 main (int argc, char *argv [])
 {
-	int primes [] = { 1, 2, 3, 5, 7, 11, 13 };
+	int primes [] = { 1, 2, 3, 5, 7, 11, 13, 17 };
 	MonoThreadInfoCallbacks thread_callbacks;
 	thread_data_t thread_data [NUM_THREADS];
-	int i;
+	int i, result, stop_count;
 
 	memset (&thread_callbacks, 0, sizeof (thread_callbacks));
 
@@ -129,25 +182,46 @@ main (int argc, char *argv [])
 	mono_thread_smr_init ();
 	mono_lls_init (&lls, free_node);
 
+	set_signal_handler ();
+
 	for (i = 0; i < N; ++i) {
 		nodes [i].node.key = i;
 		nodes [i].state = STATE_OUT;
+		nodes [i].tid = -1;
 	}
 
 	for (i = 0; i < NUM_THREADS; ++i) {
-		int result;
+		int j;
 
+		for (j = 0; j < LOCAL_NODES_PER_THREAD; ++j)
+			nodes [i * LOCAL_NODES_PER_THREAD + j].tid = i;
+	}
+
+	for (i = 0; i < NUM_THREADS; ++i) {
+		thread_data [i].tid = i;
 		thread_data [i].num_adds = thread_data [i].num_removes = 0;
-		thread_data [i].skip = primes [i];
+		thread_data [i].skip = primes [i] % N;
 		result = pthread_create (&thread_data [i].thread, NULL, worker, &thread_data [i]);
 		assert (!result);
 	}
 
-	for (i = 0; i < NUM_THREADS; ++i) {
-		int result = pthread_join (thread_data [i].thread, NULL);
-		assert (!result);
-		printf ("thread %d  adds %d  removes %d\n", i, thread_data [i].num_adds, thread_data [i].num_removes);
+	i = 0;
+	stop_count = 0;
+	while (pthread_tryjoin_np (thread_data [0].thread, NULL)) {
+		g_usleep (100000);
+		pthread_kill (thread_data [i].thread, SIGXCPU);
+		i = (i + 1) % NUM_THREADS;
+		++stop_count;
 	}
+
+	for (i = 1; i < NUM_THREADS; ++i) {
+		result = pthread_join (thread_data [i].thread, NULL);
+		assert (!result);
+	}
+
+	for (i = 0; i < NUM_THREADS; ++i)
+		printf ("thread %d  adds %d  removes %d\n", i, thread_data [i].num_adds, thread_data [i].num_removes);
+	printf ("stopped %d times\n", stop_count);
 
 	return 0;
 }
