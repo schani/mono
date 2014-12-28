7fd9313da71d86e99f0bbdefde0cc637cd1ab725
Mark Probst <mark.probst@gmail.com>
2014-12-27 20:06:26 -0800

    [sgen] Use pointer array for pin stats instead of linked list.

--- mono/metadata/sgen-gc.h
+++ mono/metadata/sgen-gc.h
@@ -437,13 +437,6 @@ void sgen_init_internal_allocator (void);
 
 #include "metadata/sgen-client-mono.h"
 
-/* FIXME: get rid of this in favor of pointer queues! */
-typedef struct _ObjectList ObjectList;
-struct _ObjectList {
-	GCObject *obj;
-	ObjectList *next;
-};
-
 typedef void (*CopyOrMarkObjectFunc) (void**, SgenGrayQueue*);
 typedef void (*ScanObjectFunc) (char *obj, mword desc, SgenGrayQueue*);
 typedef void (*ScanVTypeFunc) (char*, mword desc, SgenGrayQueue* BINARY_PROTOCOL_ARG (size_t size));
